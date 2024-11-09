e7b55ae0b608f5570eddaabf10d30b35711624d2
riedhampattel <riedhampattel.tops@gmail.com>
2024-11-09 08:48:03 +0530

    updated

--- 3.sql
+++ 3.sql
@@ -31,3 +31,39 @@ select * from employees where first_name like '%e%e%';
 select * from employees where hire_date like '%-J_n-%';
 
 select * from employees where hire_date like '%-Ju_-%';
+
+select * from employees where salary>=10000 and salary<=20000;
+
+select * from employees where salary between 10000 and 20000;
+
+select * from employees where department_id in (90,60,50);
+
+select * from employees where department_id not in (90,60,50);
+
+select * from employees;
+
+select * from employees where (department_id,manager_id) in ((90,100),(60,103));
+
+select * from employees where (department_id,manager_id) not in ((90,100),(60,103));
+
+select min(salary) from employees;
+
+select max(salary) from employees;
+
+select sum(salary) from employees;
+
+select avg(salary) from employees;
+
+select count(manager_id) from employees;
+
+select count(commission_pct) from employees;
+
+select department_id,min(salary) from employees group by department_id;
+
+select department_id,min(salary) from employees group by department_id having min(salary)>10000;
+
+select department_id,max(salary) from employees group by department_id having max(salary)>10000 order by 2;
+
+select * from employees order by salary desc limit 3;
+
+select * from employees order by salary limit 10 offset 5;
